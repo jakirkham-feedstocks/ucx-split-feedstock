fa5d9dd7d7c840813316d623516a5aca688e5af2
Peter Andreas Entschev <peter@entschev.com>
2023-04-21 07:02:26 -0700

    Fix mixed CUDA version builds

--- recipe/install_ucx.sh
+++ recipe/install_ucx.sh
@@ -2,15 +2,18 @@
 
 set -xeuo pipefail
 
-[[ ${target_platform} == "linux-64" ]] && targetsDir="targets/x86_64-linux"
-[[ ${target_platform} == "linux-ppc64le" ]] && targetsDir="targets/ppc64le-linux"
-[[ ${target_platform} == "linux-aarch64" ]] && targetsDir="targets/sbsa-linux"
-
-export CFLAGS="${CFLAGS} -I${PREFIX}/${targetsDir}/include -L${PREFIX}/${targetsDir}/lib/stubs"
-
 EXTRA_ARGS=""
-if [ "${cuda_compiler_version}" != "None" ]; then
-    EXTRA_ARGS="${EXTRA_ARGS} --with-cuda=${PREFIX}"
+
+if [ "${cuda_compiler_version}" =~ 12.* ]; then
+  EXTRA_ARGS="${EXTRA_ARGS} --with-cuda=${PREFIX}"
+
+  [[ ${target_platform} == "linux-64" ]] && targetsDir="targets/x86_64-linux"
+  [[ ${target_platform} == "linux-ppc64le" ]] && targetsDir="targets/ppc64le-linux"
+  [[ ${target_platform} == "linux-aarch64" ]] && targetsDir="targets/sbsa-linux"
+
+  export CFLAGS="${CFLAGS} -I${PREFIX}/${targetsDir}/include -L${PREFIX}/${targetsDir}/lib/stubs"
+elif [ "${cuda_compiler_version}" != "None" ]; then
+  EXTRA_ARGS="${EXTRA_ARGS} --with-cuda=${CUDA_HOME}"
 fi
 
 
