e9ed63a54164db4ef35e33c687ef59eab81f1939
Leo Fang <leo80042@gmail.com>
2024-04-25 20:37:14 -0400

    Update and rename cudart.txt to post-link-cudart.sh

--- recipe/post-link-cudart.sh
+++ recipe/post-link-cudart.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+
+cat << EOF >> $PREFIX/.messages.txt
+
 To enable CUDA support, UCX requires the presence of CUDA Runtime (libcudart).
 For CUDA 11 and below, this means the "cudatoolkit" package should be installed
 in your conda environment. For CUDA 12 and above, it is "cuda-cudart". The required
 package needs to be installed manually (versions can be set by also installing
 the "cuda-version" package, e.g. "conda install cuda-version=12.3 cuda-cudart").
+
+EOF
