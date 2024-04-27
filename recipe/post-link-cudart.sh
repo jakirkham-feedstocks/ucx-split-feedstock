a26fb36a6a832de51a1d4c0a4bbd919361f04bb5
jakirkham <jakirkham@gmail.com>
2024-04-26 18:05:01 -0700

    Put `cuda-version` last if users want to tweak it
    Makes it easier to copy-paste and then add `.X` if needed.

--- recipe/post-link-cudart.sh
+++ recipe/post-link-cudart.sh
@@ -5,7 +5,7 @@ cat << EOF >> $PREFIX/.messages.txt
 To enable CUDA support, UCX requires the CUDA Runtime library (libcudart).
 The library can be installed with the appropriate command below:
 
-* For CUDA 11, run:    conda install cuda-version=11 cudatoolkit
-* For CUDA 12, run:    conda install cuda-version=12 cuda-cudart
+* For CUDA 11, run:    conda install cudatoolkit cuda-version=11
+* For CUDA 12, run:    conda install cuda-cudart cuda-version=12
 
 EOF
